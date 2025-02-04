2accc16c3b672995666f2a25e8fe95484189109b
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-02-04 14:22:12 -0300

    feat: os componentes foram integrados em App.jsx, onde estão as requisições que o cliente deve enviar ao backend, utilizei o axios pra enviar esses requests ao servidor e para mudar o estado das tarefas com a resposta recebida

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "./components/Header";
+import TaskList from "./components/TaskList";
 
 const App = () => {
-    return <Header />;
+    const [tasks, setTasks] = useState([]);
+
+    const getTasks = async () => {
+        try {
+            const response = await axios.get("http://localhost:3600/tarefas");
+            setTasks(response.data);
+        } catch (error) {
+            console.error("Erro ao buscar as tarefas: ", error);
+        }
+    };
+
+    const addTask = async (title) => {
+        try {
+            const response = await axios.post("http://localhost:3600/tarefas", { title });
+            setTasks([...tasks, response.data.task]);
+        } catch (error) {
+            console.error("Erro ao criar tarefa: ", error);
+        }
+    };
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    return (
+        <>
+            <Header onAddTask={addTask} />
+            <TaskList tasks={tasks} />
+        </>
+    );
 };
 
 export default App;
