32f8d131db5257b809146533c16ccb9283c2d4e7
GabeMike52 <gmiqueiascarneiro52@gmail.com>
2025-02-12 17:31:16 -0300

    feat: adicionado toast para sucesso na criação de tarefa, ainda não testado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify"
 import axios from "axios";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
 
 const App = () => {
     const [tasks, setTasks] = useState([]);
+
+    const notify = (message) => toast(message);
 
     const getTasks = async () => {
         try {
@@ -19,6 +22,8 @@ const App = () => {
         try {
             const response = await axios.post("http://localhost:3600/tarefas", { title });
             setTasks([...tasks, response.data.task]);
+            const message = response.data.message;
+            notify(message);
         } catch (error) {
             console.error("Erro ao criar tarefa: ", error);
         }
